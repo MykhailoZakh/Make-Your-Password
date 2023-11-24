2cd95d4d2e1276a1efb328f0602b7a676293e5c3
Mykhailo <sharkon90@gmail.com>
2023-11-23 19:37:20 -0500

    adding new arreys with UpperCasse Latters, LowerCase letters, numbers, and special characters, adding stuctual coments for script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+let cLetters =[`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
+let lLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]; 
 
+let numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+let specialChars = [`!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `]`, `^`, `_`, "`", `{`, `|`, `}`, `~`];
+
+// 
+// for (i = 0; i < cLetters.length; i++) {
+//   console.log(`${cLetters[i]}, ${lLetters[i]}`);
+// }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +22,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add window prompt to ask lenght of the password 8-128 
+
+// Add window confirm for Upper Case
+
+// Add window confirm for Lower Case 
+
+// Add window confirm for Numeric 
+
+// Add windor confirm for Special Characters
