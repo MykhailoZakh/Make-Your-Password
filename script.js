6940e613765a3301eb90c5aefb9527ebbe589887
Mykhailo <sharkon90@gmail.com>
2023-11-27 15:10:08 -0500

    changing variable names in JS to make it more nicer

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Assignment Code
 
 var generateBtn = document.querySelector("#generate");
-let cLetters =[`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
-let lLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]; 
+// Strings with different characters for password
+let capitalLetters =[`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
+
+let lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]; 
 
 let numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-let specialChars = [`!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `]`, `^`, `_`, "`", `{`, `|`, `}`, `~`];
+let specialCharacters = [`!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, `-`, `.`, `/`, `:`, `;`, `<`, `=`, `>`, `?`, `@`, `[`, `]`, `^`, `_`, "`", `{`, `|`, `}`, `~`];
 
 
 
@@ -15,60 +17,61 @@ let specialChars = [`!`, `"`, `#`, `$`, `%`, `&`, `'`, `(`, `)`, `*`, `+`, `,`, 
 // Write password to the #password input
 function writePassword() {
   // Add window prompt to ask length of the password 8-128 
-  let passwLength = window.prompt(`What is your password length? Choose from 8 to 128.`);
+  let passwordLength = window.prompt(`What is your password length? Chose from 8 to 128.`);
   // Logic to check if password length is acceptable
-  if ((Number(passwLength) < 8) || (Number(passwLength) > 128)) {
-  window.alert(`Wrong Password Length. Please choose from 8 to 128!! 🤷‍♀️🤷‍♂️`);
+  if ((parseInt(passwordLength) < 8) || (parseInt(passwordLength) > 128)) {
+  window.alert(`Wrong Password Length. Please chose from 8 to 128!! 🙌📑`);
   return;
 }
   // Add window confirm for Upper Case
-  let hasCLetters = window.confirm(`Do you want Capital Letters in your password?`);
+  let hasCapitalLetters = window.confirm(`Do you want Capital Letters in your password?`);
 
   // Add window confirm for Lower Case 
-  let hasLLetters = window.confirm(`Do you want Lower Case Letters in your password?`);
+  let hasLowerCaseLetters = window.confirm(`Do you want Lower Case Letters in your password?`);
 
   // Add window confirm for Numeric 
   let hasNumeric = window.confirm(`Do you want Numbers in your password?`);
 
   // Add windor confirm for Special Characters
-  let hasSecialChars = window.confirm(`Do you want Special Characters in your password?`);
+  let hasSecialCharacters = window.confirm(`Do you want Special Characters in your password?`);
 
   // Logic to check if user choose at least one condition
-  if((!hasCLetters && !hasLLetters) && (!hasNumeric && !hasSecialChars)) {
+  if((!hasCapitalLetters && !hasLowerCaseLetters) && (!hasNumeric && !hasSecialCharacters)) {
     window.alert(`You need to chose at least one condition to create Your Password!! 🤷‍♂️🤷‍♀️ Please try again! 😊`)
     return ;
   }
 
-  console.log(passwLength, hasCLetters, hasLLetters, hasNumeric, hasSecialChars);
+  // console.log(passwordLength, hasCapitalLetters, hasLowerCaseLetters, hasNumeric, hasSecialCharacters);
 
   // var password = generatePassword();
-  let newPassArr = [];
-  let newPass = ``;
-  //  Logisc to check what kind of symbols need to be included in password
-  for(i=0; i < passwLength; i++) {
-      if(hasCLetters) {
-        newPassArr.push(cLetters[Math.floor(Math.random() * cLetters.length)]);
+  let newPasswordArray = [];
+  let newPassword = ``;
+  //  Loop to check what kind of symbols need to be included in password
+  for(i=0; i < passwordLength; i++) {
+      if(hasCapitalLetters) {
+        newPasswordArray.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
       }
-      if(hasLLetters) {
-        newPassArr.push(lLetters[Math.floor(Math.random() * lLetters.length)]);
+      if(hasLowerCaseLetters) {
+        newPasswordArray.push(lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]);
       }
       if(hasNumeric) {
-        newPassArr.push(numeric[Math.floor(Math.random() * numeric.length)]);
+        newPasswordArray.push(numeric[Math.floor(Math.random() * numeric.length)]);
       }
-      if(hasSecialChars){
-        newPassArr.push(specialChars[Math.floor(Math.random() * specialChars.length)]);
+      if(hasSecialCharacters){
+        newPasswordArray.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
       }
     }
   
-  for(i=0; i < passwLength; i++){
-      newPass += `${newPassArr[Math.floor(Math.random() * newPassArr.length)]}`;
+    //  Loop to add chosen characters to newPassword string
+  for(i=0; i < passwordLength; i++){
+      newPassword += `${newPasswordArray[Math.floor(Math.random() * newPasswordArray.length)]}`;
   }
-    console.log(newPass);
+    // console.log(newPassword);
     
-  
+  //  Selection of the text area id and declaration a new variable for it
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = newPass;
+  //  Adding a new string with new password that we created to a variable  
+  passwordText.value = newPassword;
 
 }
 
